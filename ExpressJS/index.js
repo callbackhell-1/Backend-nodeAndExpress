916c56b4af27bf7611fb8208c57b0874a9e1b2c8
Adarsh <jha01adarsh@gmail.com>
2023-03-18 01:02:57 +0530

    View engine setup

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -4,26 +4,30 @@ const path = require("path");
 
 const app = express();
 
-const dirPath = path.join(__dirname, "public");
+// const dirPath = path.join(__dirname, "public");
 // console.log(dirPath);
+// app.use(express.static(dirPath));
 
-app.use(express.static(dirPath));
+// Template engine
+app.set("view engine", "ejs");
+// console.log(app.get("view engine")); //ejs
+// console.log(__dirname,"views");
+
 
 app.get("/", (req, res) => {
-  res.sendFile(`${dirPath}/index.html`);
+  //   res.sendFile(`${dirPath}/index.html`);
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(`${dirPath}/about.html`);
+  //   res.sendFile(`${dirPath}/about.html`);
 });
 
 app.get("/contact", (req, res) => {
-  res.send(`<h1>Thanks for contacting us</h1>`);
+  //   res.send(`<h1>Thanks for contacting us</h1>`);
 });
 
-// convert object data into JSON data in Browser.
 app.get("/download", (req, res) => {
-  res.download(`${dirPath}/index.html`);
+  //   res.download(`${dirPath}/index.html`);
 });
 
 app.listen(port, (err) => {
