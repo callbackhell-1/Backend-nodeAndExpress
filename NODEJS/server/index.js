552644cfe458f8394d7bdb99bdda218cf53ecf81
Adarsh <jha01adarsh@gmail.com>
2023-03-17 11:11:23 +0530

    Folder arranged

---
+++ NODEJS/server/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("hi this is my first nodejs server");
+  } else if (req.url === "/download") {
+    res.end("hi this is my first download page");
+  } else {
+    res.end("404: Page could not be found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server Listening on port  no. 3000");
+  console.log(__dirname);
+});
