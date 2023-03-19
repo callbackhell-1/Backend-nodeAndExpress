e6a26becad47b5d9ff934948f2404cb19cd2070e
Adarsh <jha01adarsh@gmail.com>
2023-03-20 02:38:15 +0530

    send plaintext,status,json,status+data

--- Express-JS/index.js
+++ Express-JS/index.js
@@ -1,12 +1,25 @@
 import express from "express";
 
-// create server
 const app = express();
-// console.log(app);
 
-// Listen server
+app.get("/", (req, res) => {
+    
+  //   res.send("Hello from express");
+
+  //   res.sendStatus(404);
+  //   res.json({
+  //     success: true,
+  //     products: [],
+  //   });
+
+  //   res.sendStatus(400);
+
+  res.status(400).send("This will show");
+});
+
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
 
 // npm run dev
+// status code read
