c0bbb343df5a280e60011817022224fe30858d3c
Adarsh <jha01adarsh@gmail.com>
2023-03-20 01:56:28 +0530

    fs module Async

--- NODE-JS/index.js
+++ NODE-JS/index.js
@@ -1,19 +1,18 @@
 import http from "http";
 import { percent } from "./feature.js";
-
-console.log(percent);
-console.log(percent());
-
-/*
-[Function: percent]
-57
-*/
+import fs from "fs";
 
 const server = http.createServer((req, res) => {
   // routing
-
   if (req.url == "/") {
-    res.end("<h1>Welcome to Home page</h1>");
+    fs.readFile("./index.html", (err, data) => {
+      if (err) {
+        console.log("Error", err);
+        return;
+      }
+      res.end(data);
+      console.log("File readed");
+    });
   } else if (req.url == "/about") {
     res.end(`<h1>Welcome to About page and percent is ${percent()}</h1>`);
   } else if (req.url == "/contact") {
