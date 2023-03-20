579adfd19c79329cde782b3283315a57c3973f76
Adarsh <jha01adarsh@gmail.com>
2023-03-20 12:13:37 +0530

    Serving dynamic data

--- Express-JS/index.js
+++ Express-JS/index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import fs from "fs";
-import path from "path";
 
 const app = express();
 
@@ -8,22 +6,26 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    name: "John",
+    city: "LA",
+    country: "Ind",
+  });
 });
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
 
+/*  Note : */
 
 /* 
--- for render we use  view(ejs)
+-- Local means whole object :{name: "John",city: "LA",country: "Ind"}
 
--- either set view engine : app.set("view engine", "ejs"); 
+-- If we use local.name , result will be same
 
-"or"
---  res.render("index.ejs"); (give extension also)
+--  We use render for dynamic data
 
---If we set view engine no need to give ext.
+--
 
 */
