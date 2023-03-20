7c271ce8f7a5566fd3e640fe428ee81ce9a3311b
Adarsh <jha01adarsh@gmail.com>
2023-03-20 11:57:33 +0530

    Setting up view engine and rendering file

--- Express-JS/index.js
+++ Express-JS/index.js
@@ -1,21 +1,29 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  const pathLoc = path.resolve();
+// setting up view engine
+app.set("view engine", "ejs");
 
-  console.log(pathLoc);
-  /* C:\Users\Epitome\Desktop\Backend-nodeAndExpress\Express-JS
-   */
-  res.sendFile(path.join(pathLoc, "./index.html"));
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
 
-// npm run dev
 
-// Path.resolve() == __dirname("location")
+/* 
+-- for render we use  view(ejs)
+
+-- either set view engine : app.set("view engine", "ejs"); 
+
+"or"
+--  res.render("index.ejs"); (give extension also)
+
+--If we set view engine no need to give ext.
+
+*/
