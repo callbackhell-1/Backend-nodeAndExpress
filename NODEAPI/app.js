d230b982337d01adcf868f1fc305ea736970da9e
Adarsh <jha01adarsh@gmail.com>
2023-03-27 02:39:05 +0530

    Router setup

--- NODEAPI/app.js
+++ NODEAPI/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
 
 const port = 3000;
 
@@ -34,42 +33,6 @@ app.use(express.json()); // as we are sending JSON so we use this
 app.get("/", (req, res) => {
   res.send("<h1>Hi I'm Groot</h1>");
 });
-
-app.get("/users/all", async (req, res) => {
-  // find all user from db
-  const users = await User.find({});
-
-  res.json({
-    success: true,
-    users,
-  });
-});
-
-// create new user
-app.post("/users/new", async (req, res) => {
-  const { name, email, password } = req.body;
-  // user create
-  await User.create({
-    name,
-    email,
-    password,
-  });
-  // sending response
-  res.status(201).cookie("ctoken", "tokenval").json({
-    success: true,
-    message: "Registered",
-  });
-});
-
-app.get("/userid/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findById(id);
-  res.json({
-    success: true,
-    user,
-  });
-});
-
 
 app.listen(port, (err) => {
   if (err) {
--- NODEAPI/routes/user.js
+++ NODEAPI/routes/user.js
@@ -2,4 +2,41 @@ import express from "express";
 
 const router = express.Router();
 
+// Display all user
+router.get("/users/all", async (req, res) => {
+  // find all user from db
+  const users = await User.find({});
+
+  res.json({
+    success: true,
+    users,
+  });
+});
+
+// create new user
+router.post("/users/new", async (req, res) => {
+  const { name, email, password } = req.body;
+  // user create
+  await User.create({
+    name,
+    email,
+    password,
+  });
+  // sending response
+  res.status(201).cookie("ctoken", "tokenval").json({
+    success: true,
+    message: "Registered",
+  });
+});
+
+// Detail of user Based in ID
+app.get("/userid/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  res.json({
+    success: true,
+    user,
+  });
+});
+
 export default router;
