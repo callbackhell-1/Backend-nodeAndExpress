3ce413caebec78234e73a95789fc86077b5fe2da
Adarsh <jha01adarsh@gmail.com>
2023-03-26 18:56:57 +0530

    Basic route & db connect

--- NODEAPI/app.js
+++ NODEAPI/app.js
@@ -2,9 +2,32 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
+// DB connection
+mongoose
+  .connect("mongodb://localhost:27017", {
+    dbName: "BACKENDAPI",
+  })
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((error) => {
+    console.log("Not Connected to db", error);
+  });
+
 const port = 3000;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hi I'm Groot</h1>");
+});
+
+app.get("/users/all", (req, res) => {
+  res.json({
+    success: true,
+    user: [],
+  });
+});
 
 app.listen(port, (err) => {
   if (err) {
