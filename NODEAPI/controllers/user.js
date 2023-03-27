2e8beac878d42aa7f69df88700601c2a58a8a294
Adarsh <jha01adarsh@gmail.com>
2023-03-28 01:08:39 +0530

    controllers organised

--- NODEAPI/controllers/user.js
+++ NODEAPI/controllers/user.js
@@ -9,3 +9,29 @@ export const getAllUsers = async (req, res) => {
     users,
   });
 };
+
+// create new user
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  // user create
+  await User.create({
+    name,
+    email,
+    password,
+  });
+  // sending response
+  res.status(201).cookie("ctoken", "tokenval").json({
+    success: true,
+    message: "Registered",
+  });
+};
+
+// user details
+export const getUserDetails = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  res.json({
+    success: true,
+    user,
+  });
+};
--- NODEAPI/routes/user.js
+++ NODEAPI/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { User } from "../models/user.js";
-import {getAllUsers} from "../controllers/user.js";
+import { getAllUsers, register, getUserDetails } from "../controllers/user.js";
 
 const router = express.Router();
 
@@ -8,29 +8,9 @@ const router = express.Router();
 router.get("/all", getAllUsers);
 
 // create new user
-router.post("/new", async (req, res) => {
-  const { name, email, password } = req.body;
-  // user create
-  await User.create({
-    name,
-    email,
-    password,
-  });
-  // sending response
-  res.status(201).cookie("ctoken", "tokenval").json({
-    success: true,
-    message: "Registered",
-  });
-});
+router.post("/new", register);
 
 // Detail of user Based in ID
-router.get("/userid/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findById(id);
-  res.json({
-    success: true,
-    user,
-  });
-});
+router.get("/userid/:id", getUserDetails);
 
 export default router;
