4c2a14302fc3bd1918e9b36a9a2edd24f3dbfa0c
Adarsh <jha01adarsh@gmail.com>
2023-04-06 02:04:54 +0530

    my all task

--- TODO Backend/controller/task.js
+++ TODO Backend/controller/task.js
@@ -14,3 +14,18 @@ export const newTask = async (req, res, next) => {
     message: "Task created !",
   });
 };
+
+// my task
+export const getMyTask = async (req, res, next) => {
+  // TODO:  show task of user who logged in
+  // get user id
+  const userId = req.user._id;
+
+  // find all task of user whose id we got/who's logged in
+  const tasks = await Task.find({ user: userId });
+
+  res.status(200).json({
+    success:true,
+    tasks, // array
+  })
+};
--- TODO Backend/routes/task.js
+++ TODO Backend/routes/task.js
@@ -1,11 +1,13 @@
-import express from "express"
-import { newTask } from "../controller/task.js";
+import express from "express";
+import { getMyTask, newTask } from "../controller/task.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-
 
 const router = express.Router();
 
 // Adding new task
-router.post("/new",isAuthenticated,newTask);
+router.post("/new", isAuthenticated, newTask);
+
+// get me/user task
+router.get("/my", isAuthenticated, getMyTask);
 
 export default router;
