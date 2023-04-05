cdfa0abc6d69761699390efadb139231c838cdb4
Adarsh <jha01adarsh@gmail.com>
2023-04-05 18:43:21 +0530

    another way to create task

--- TODO Backend/controller/task.js
+++ TODO Backend/controller/task.js
@@ -1,3 +1,13 @@
-export const newTask = (req, res, next) => {
-  
+import { Task } from "../models/task";
+
+export const newTask = async (req, res, next) => {
+  const { title, description } = req.body;
+
+  await Task.create({ title, description });
+
+  /**
+   * 1 more way to create is:
+   * const task = new Task({title});
+   * await task.save();
+   */
 };
