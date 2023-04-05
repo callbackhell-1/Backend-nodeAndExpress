136f696af3a4b60ac420f2e659c768309384e850
Adarsh <jha01adarsh@gmail.com>
2023-04-06 00:19:35 +0530

    Task create in DB

--- TODO Backend/controller/task.js
+++ TODO Backend/controller/task.js
@@ -3,11 +3,9 @@ import { Task } from "../models/task";
 export const newTask = async (req, res, next) => {
   const { title, description } = req.body;
 
-  await Task.create({ title, description });
-
-  /**
-   * 1 more way to create is:
-   * const task = new Task({title});
-   * await task.save();
-   */
+  await Task.create({
+    title,
+    description,
+    user: req.user, //only logged in user can add task
+  });
 };
--- TODO Backend/routes/task.js
+++ TODO Backend/routes/task.js
@@ -1,8 +1,11 @@
 import express from "express"
 import { newTask } from "../controller/task.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+
 const router = express.Router();
 
 // Adding new task
-router.post("/new",newTask);
+router.post("/new",isAuthenticated,newTask);
 
 export default router;
