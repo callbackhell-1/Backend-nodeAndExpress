a27bda1d6b2d672b239931962fbc323a8f63f4cf
Adarsh <jha01adarsh@gmail.com>
2023-04-08 16:06:35 +0530

    Try catch in TAsk controller

--- TODO Backend/controller/task.js
+++ TODO Backend/controller/task.js
@@ -2,74 +2,90 @@ import ErrorHandler from "../middlewares/error.js";
 import { Task } from "../models/task.js";
 
 export const newTask = async (req, res, next) => {
-  const { title, description } = req.body;
+  try {
+    const { title, description } = req.body;
 
-  await Task.create({
-    title,
-    description,
-    user: req.user, //only logged in user can add task
-  });
+    await Task.create({
+      title,
+      description,
+      user: req.user, //only logged in user can add task
+    });
 
-  res.status(201).json({
-    success: true,
-    message: "Task created !",
-  });
+    res.status(201).json({
+      success: true,
+      message: "Task created !",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // my task
 export const getMyTask = async (req, res, next) => {
-  //   show task of user who logged in
-  // get user id
-  const userId = req.user._id;
+  try {
+    //   show task of user who logged in
+    // get user id
+    const userId = req.user._id;
 
-  // find all task of user whose id we got/who's logged in
-  const tasks = await Task.find({ user: userId });
+    // find all task of user whose id we got/who's logged in
+    const tasks = await Task.find({ user: userId });
 
-  res.status(200).json({
-    success: true,
-    tasks, // array
-  });
+    res.status(200).json({
+      success: true,
+      tasks, // array
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Update task
 
 export const updateTask = async (req, res, next) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  // find task with that id
-  const task = await Task.findById(id);
+    // find task with that id
+    const task = await Task.findById(id);
 
-  // if no task
-  if (!task) {
-    return next(new ErrorHandler("Invalid Id", 404));
+    // if no task
+    if (!task) {
+      return next(new ErrorHandler("Invalid Id", 404));
+    }
+
+    //update
+    task.isCompleted = !task.isCompleted; //making opposite , if true then false, vice-versa
+
+    await task.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Task Updated !",
+    });
+  } catch (error) {
+    next(error);
   }
-
-  //update
-  task.isCompleted = !task.isCompleted; //making opposite , if true then false, vice-versa
-
-  await task.save();
-
-  res.status(200).json({
-    success: true,
-    message: "Task Updated !",
-  });
 };
 
 // Delete task
 export const deleteTask = async (req, res, next) => {
-  // find task with that id
-  const task = await Task.findById(req.params.id);
+  try {
+    // find task with that id
+    const task = await Task.findById(req.params.id);
 
-  // if no task
-  if (!task) {
-    return next(new ErrorHandler("Invalid Id", 404));
+    // if no task
+    if (!task) {
+      return next(new ErrorHandler("Invalid Id", 404));
+    }
+
+    // removing task
+    await task.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Task Deleted",
+    });
+  } catch (error) {
+    next(error);
   }
-
-  // removing task
-  await task.deleteOne();
-
-  res.status(200).json({
-    success: true,
-    message: "Task Deleted",
-  });
 };
